637e9ae41eadb0e3ad33d5500f6cd5b6563d59b1
aadeshdhakal <aadeshdhakal@gmail.com>
2017-12-10 15:11:01 +0545

    v = u+at

---
+++ acceleration.py
@@ -0,0 +1,10 @@
+v = 25
+u = 0
+t = 10
+a = (v-u)/t
+
+print("We have,\n\t v = u + at")
+print("or, {0} = {1} + a{2}".format(v, u, t))
+print("or, a = ({0}-{1})/{2}".format(v, u, t))
+print("hence, a = {0}".format(a))
+
